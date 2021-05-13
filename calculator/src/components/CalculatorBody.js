84044d4357b075fb1833eda9c12491a759d86686
Michael Prescott <michael_prescott@baylor.edu>
2021-05-13 11:59:06 -0500

    Added view window in calculator

---
+++ calculator/src/components/CalculatorBody.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CalculatorBody.css'
+
+function CalculatorBody() {
+    return (
+        <div className="CalcBody">
+            
+        </div>
+    )
+}
+
+export default CalculatorBody
